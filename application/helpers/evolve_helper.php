6aa3b9de3906ed6e4799928e844bdb839a35a929
Fahry Lafir <fahrydgt@gmail.com>
2019-07-31 13:44:07 +0530

    hdjsQucEntryMemo

--- application/helpers/evolve_helper.php
+++ application/helpers/evolve_helper.php
@@ -77,30 +77,62 @@ if ( ! function_exists('gen_id'))
 
 
 // Encrypt Function
-function mc_encrypt($encrypt, $key){
-    $encrypt = serialize($encrypt);
-    $iv = mcrypt_create_iv(mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_CBC), MCRYPT_DEV_URANDOM);
-    $key = pack('H*', $key);
-    $mac = hash_hmac('sha256', $encrypt, substr(bin2hex($key), -32));
-    $passcrypt = mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $key, $encrypt.$mac, MCRYPT_MODE_CBC, $iv);
-    $encoded = base64_encode($passcrypt).'|'.base64_encode($iv);
-    return $encoded;
+ function mc_encrypt($string,$secret_key) {//support php 7
+    $output = false;
+    $encrypt_method = "AES-256-CBC"; 
+    $secret_iv = 'zone_venture_fl';
+    // hash
+    $key = hash('sha256', $secret_key);
+    
+    // iv - encrypt method AES-256-CBC expects 16 bytes - else you will get a warning
+    $iv = substr(hash('sha256', $secret_iv), 0, 16);
+    
+    $output = openssl_encrypt($string, $encrypt_method, $key, 0, $iv);
+    $output = base64_encode($output);
+    
+    return $output;
 }
+
+//function mc_encrypt($encrypt, $key){ //support php 5.5
+//    $encrypt = serialize($encrypt);
+//    $iv = mcrypt_create_iv(mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_CBC), MCRYPT_DEV_URANDOM);
+//    $key = pack('H*', $key);
+//    $mac = hash_hmac('sha256', $encrypt, substr(bin2hex($key), -32));
+//    $passcrypt = mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $key, $encrypt.$mac, MCRYPT_MODE_CBC, $iv);
+//    $encoded = base64_encode($passcrypt).'|'.base64_encode($iv);
+//    return $encoded;
+//}
 // Decrypt Function
-function mc_decrypt($decrypt, $key){
-    $decrypt = explode('|', $decrypt.'|');
-    $decoded = base64_decode($decrypt[0]);
-    $iv = base64_decode($decrypt[1]);
-    if(strlen($iv)!==mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_CBC)){ return false; }
-    $key = pack('H*', $key);
-    $decrypted = trim(mcrypt_decrypt(MCRYPT_RIJNDAEL_256, $key, $decoded, MCRYPT_MODE_CBC, $iv));
-    $mac = substr($decrypted, -64);
-    $decrypted = substr($decrypted, 0, -64);
-    $calcmac = hash_hmac('sha256', $decrypted, substr(bin2hex($key), -32));
-    if($calcmac!==$mac){ return false; }
-    $decrypted = unserialize($decrypted);
-    return $decrypted;
+ 
+ function mc_decrypt($string,$secret_key) { //support php 7
+    $output = false;
+    $encrypt_method = "AES-256-CBC"; 
+    $secret_iv = 'zone_venture_fl';
+    // hash
+    $key = hash('sha256', $secret_key);
+    
+    // iv - encrypt method AES-256-CBC expects 16 bytes - else you will get a warning
+    $iv = substr(hash('sha256', $secret_iv), 0, 16);
+    
+    $output = openssl_decrypt(base64_decode($string), $encrypt_method, $key, 0, $iv); 
+    
+    return $output; 
 }
+//function mc_decrypt($decrypt, $key){ //support php 5.5
+//    $decrypt = explode('|', $decrypt.'|');
+//    $decoded = base64_decode($decrypt[0]);
+//    $iv = base64_decode($decrypt[1]);
+//    if(strlen($iv)!==mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_CBC)){ return false; }
+//    $key = pack('H*', $key);
+//    $decrypted = trim(mcrypt_decrypt(MCRYPT_RIJNDAEL_256, $key, $decoded, MCRYPT_MODE_CBC, $iv));
+//    $mac = substr($decrypted, -64);
+//    $decrypted = substr($decrypted, 0, -64);
+//    $calcmac = hash_hmac('sha256', $decrypted, substr(bin2hex($key), -32));
+//    if($calcmac!==$mac){ return false; }
+//    $decrypted = unserialize($decrypted);
+//    return $decrypted;
+//}
+
 
 function fl_image_resizer($file_name,$target_width,$target_height,$resized_name='', $source_dir='',$destn_dir=''){
     $file = $source_dir.$file_name;
